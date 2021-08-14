78fa6ab71dd303ec49fe5fb147f15c95afccf3b4
Amara <jollybabe1@gmail.com>
2021-08-14 18:21:03 +0100

    My portfolio

--- forms/contact.php
+++ forms/contact.php
@@ -9,15 +9,21 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'sandraamaraeze@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  /**if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
   $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
+  $contact->ajax = true;**/
+
+  if if(mail($contact->to, $contact->subject, $contact->add_message, $headers)){ 
+    echo 'Email has sent successfully.'; 
+ }else{ 
+    echo 'Email sending failed.'; 
+ }
+  $headers = 'From: '.$contact->from_name.'<'.$contact->from_email.'>'; 
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
